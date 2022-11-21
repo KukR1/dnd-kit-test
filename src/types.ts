0046968fcb9e73bf6a0337d424e1b06919d1c565
KukR1 <nikdani123@hotmail.com>
2022-11-21 11:05:32 +0100

    Latest changes

---
+++ src/types.ts
@@ -0,0 +1,27 @@
+import type { MutableRefObject } from 'react';
+import type { UniqueIdentifier } from '@dnd-kit/core';
+
+export interface TreeItem {
+  id: UniqueIdentifier;
+  children: TreeItem[];
+  collapsed?: boolean;
+  dashboards?: DashboardType[];
+}
+
+export type DashboardType = {
+  id: UniqueIdentifier;
+  title: string;
+}
+
+export type TreeItems = TreeItem[];
+
+export interface FlattenedItem extends TreeItem {
+  parentId: UniqueIdentifier | null;
+  depth: number;
+  index: number;
+}
+
+export type SensorContext = MutableRefObject<{
+  items: FlattenedItem[];
+  offset: number;
+}>;
